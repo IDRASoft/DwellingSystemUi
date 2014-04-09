150a582047c6fc1cc994585d4b0e1b9b4773e46c
Adrian Mtz <adrian.mapj@hotmail.com>
2014-04-09 18:49:57 -0500

    Se agrega funcionalidad de guardado para DwellingRel

--- DwellingSystemUi/Areas/Managment/Controllers/MainController.cs
+++ DwellingSystemUi/Areas/Managment/Controllers/MainController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Web.Mvc;
 using DwellingRepository.Common;
 using DwellingRepository.Database;
 using DwellingRepository.Models.Shared;
 using DwellingSystemUi.Controllers;
 using DwellingSystemUi.Resources;
+using DwellingSystemUi.Services;
 using Infrastructure.JqGrid.Model;
 
 namespace DwellingSystemUi.Areas.Managment.Controllers
@@ -26,88 +28,44 @@ namespace DwellingSystemUi.Areas.Managment.Controllers
             return Json(genericRepository.JqGridFindBy(opts,VwDwellingDataJson.Key,VwDwellingDataJson.Columns));
         }
 
-        public ActionResult Upsert()
+        public ActionResult Upsert(int? id)
         {
+            /*DwellingRel model = null;
+            if (id != null)
+            {
+                model = new GenericRepository<DwellingRel>(Db).FindById(id);
+
+                //model.DwellingApartmentToUse = (DwellingApartment)model.DwellingApartment.DefaultIfEmpty();
+                //model.DwellingHouseToUse = model.dwel
+            }
+            else
+            {
+                model =new DwellingRel();
+            }*/
+
             return View(new DwellingRel());
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult DoUpsert(DwellingRel model)
         {
-            try
+
+            if (ModelState.IsValid)
             {
-
-                if (model.IsApartment)
-                {
-                    if (model.DwellingApartment == null)
-                    {
-                        model.DwellingApartment = new List<DwellingApartment> {model.DwellingApartmentToUse};
-                    }
-                    else
-                    {
-                        model.DwellingApartment.Clear();
-                        model.DwellingApartment.Add(model.DwellingApartmentToUse);
-                    }
-                }
-                else
-                {
-                    if (model.DwellingHouse == null)
-                    {
-                        model.DwellingHouse = new List<DwellingHouse> { model.DwellingHouseToUse };
-                    }
-                    else
-                    {
-                        model.DwellingHouse.Clear();
-                        model.DwellingHouse.Add(model.DwellingHouseToUse);
-                    }
-                }
-               
-
-                if(model.DwellingHouseToUse.OuterNumber == null)
-                    Debug.WriteLine("es nula la cosa");
-
-                if (ModelState.IsValid)
-                {
-                    return Json(new ResponseMessageModel
-                                {
-                                    HasError = false,
-                                    Title = ResShared.TITLE_REGISTER_FAILED,
-                                    Message = ResShared.ERROR_INVALID_MODEL
-                                });
-                }
-
-                /*
-                Db.DwellingApartment.Add(model.DwellingApartmentToUse);
-
-                var repository = new GenericRepository<DwellingRel>();
-
-                if (model.DwellingId == EntityConstants.NULL_VALUE)
-                {
-                    repository.Add(model);
-                }
-                else
-                {
-                    repository.Update(model);
-                }*/
-
                 return Json(new ResponseMessageModel
                             {
-                                HasError = true,
-                                Title = ResShared.TITLE_REGISTER_SUCCESS,
-                                Message = ResShared.INFO_REGISTER_SAVED
+                                HasError = false,
+                                Title = ResShared.TITLE_REGISTER_FAILED,
+                                Message = ResShared.ERROR_INVALID_MODEL
                             });
             }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.Message);
-                return Json(new ResponseMessageModel
-                {
-                    HasError = true,
-                    Title = ResShared.TITLE_REGISTER_FAILED,
-                    Message = ResShared.ERROR_UNKOWN
-                });
-            }
+
+            var dwellingSvc = new DwellingRelService();
+
+            return Json(dwellingSvc.SaveUpdateDwellingRel(model));
         }
+
     }
 }
---
+++ DwellingSystemUi/Services/DwellingRelService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using DwellingRepository.Common;
+using DwellingRepository.Database;
+using DwellingRepository.Models.Shared;
+using DwellingSystemUi.Resources;
+
+namespace DwellingSystemUi.Services
+{
+    public class DwellingRelService
+    {
+        public ResponseMessageModel SaveUpdateDwellingRel(DwellingRel model)
+        {
+            try
+            {
+                if (model.IsApartment)
+                {
+                    if (model.DwellingApartment == null)
+                    {
+                        model.DwellingApartment = new List<DwellingApartment> { model.DwellingApartmentToUse };
+                    }
+                    else
+                    {
+                        model.DwellingApartment.Clear();
+                        model.DwellingApartment.Add(model.DwellingApartmentToUse);
+                    }
+
+                    if (model.DwellingHouse != null)
+                    {
+                        foreach (var it in model.DwellingHouse)
+                        {
+                            it.IsObsolete = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (model.DwellingHouse == null)
+                    {
+                        model.DwellingHouse = new List<DwellingHouse> { model.DwellingHouseToUse };
+                    }
+                    else
+                    {
+                        model.DwellingHouse.Clear();
+                        model.DwellingHouse.Add(model.DwellingHouseToUse);
+                    }
+
+                    if (model.DwellingApartment != null)
+                    {
+                        foreach (var it in model.DwellingApartment)
+                        {
+                            it.IsObsolete = true;
+                        }
+                    }
+                }
+
+                var repository = new GenericRepository<DwellingRel>();
+
+                if (model.DwellingId == EntityConstants.NULL_VALUE)
+                {
+                    repository.Add(model);
+                }
+                else
+                {
+                    repository.Update(model);
+                }
+
+                return new ResponseMessageModel
+                       {
+                           HasError = false,
+                           Title = ResShared.TITLE_REGISTER_SUCCESS,
+                           Message = ResShared.INFO_REGISTER_SAVED
+                       };
+
+            }
+
+            catch (Exception)
+            {
+                return new ResponseMessageModel
+                       {
+                           HasError = true,
+                           Title = ResShared.TITLE_REGISTER_FAILED,
+                           Message = ResShared.ERROR_UNKOWN
+                       };
+            }
+        }
+    }
+}
