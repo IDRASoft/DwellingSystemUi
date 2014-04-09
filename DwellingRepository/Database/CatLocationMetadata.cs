73e31bb58d93d32af84bc3ff901da26a9b07cb3a
Adrian Mtz <adrian.mapj@hotmail.com>
2014-04-09 15:46:02 -0500

    Eliminar usings sin uso

--- DwellingRepository/Database/CatLocationMetadata.cs
+++ DwellingRepository/Database/CatLocationMetadata.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DwellingRepository.Resources;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DwellingRepository.Database
 {
