784955f47ecaa3a048a758885f647748228478d2
Adrian Mtz <adrian.mapj@hotmail.com>
2014-04-15 11:56:07 -0500

    Prueba de ultima version

--- DwellingSystemUi/Areas/Managment/Controllers/ResidentController.cs
+++ DwellingSystemUi/Areas/Managment/Controllers/ResidentController.cs
@@ -8,7 +8,7 @@ using DwellingSystemUi.Resources;
 
 namespace DwellingSystemUi.Areas.Managment.Controllers
 {
-    public class ResidentController : BaseController
+    public class ResidentControlleeeeeeeeeeeeeeeer : BaseController
     {
         public ActionResult Upsert(int? id)
         {
